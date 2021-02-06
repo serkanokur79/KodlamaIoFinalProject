6e40e1e87426a4b982546d5faec14594983191de
Serkan Okur <serkan.okur@gmail.com>
2021-02-06 21:24:14 +0100

    Day09 Lesson 03 End

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,9 +11,25 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            // ProductTest();
+
+            CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (Product product in productManager.GetByUnitPrice(40, 100)) 
+            foreach (Product product in productManager.GetByUnitPrice(40, 100))
             {
                 Console.WriteLine(product.ProductName);
             }
